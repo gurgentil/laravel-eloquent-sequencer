3e1f3345f6f2e4a85d2c6e95298fc2318b1105d9
Gustavo Gentil <gustavorgentil@outlook.com>
2020-07-31 01:38:40 -0300

    test: ensure strategy set to always works on update

--- tests/Unit/StrategyAlwaysTest.php
+++ tests/Unit/StrategyAlwaysTest.php
@@ -27,6 +27,25 @@ class StrategyAlwaysTest extends TestCase
      * @test
      * @group Strategy
      */
+    public function strategy_set_to_always_works_on_update()
+    {
+        config(['eloquentsequencer.strategy' => SequencingStrategy::ALWAYS]);
+
+        $group = Factory::of('group')->create();
+
+        $firstItem = Factory::of('item')->create(['group_id' => $group->id]);
+        $secondItem = Factory::of('item')->create(['group_id' => $group->id]);
+
+        $secondItem->update(['position' => 1]);
+
+        $this->assertEquals(2, $firstItem->refresh()->position);
+        $this->assertEquals(1, $secondItem->refresh()->position);
+    }
+
+    /**
+     * @test
+     * @group Strategy
+     */
     public function strategy_set_to_always_works_on_delete()
     {
         config(['eloquentsequencer.strategy' => SequencingStrategy::ALWAYS]);
