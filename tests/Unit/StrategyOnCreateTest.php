c90aef4fbf3a7c4923eab68bea9dea63af4e6193
Gustavo Gentil <gustavorgentil@outlook.com>
2020-07-31 02:37:16 -0300

    test: ensure strategy set on_create and on_update work

---
+++ tests/Unit/StrategyOnCreateTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Gurgentil\LaravelEloquentSequencer\Tests\Unit;
+
+use Facades\Gurgentil\LaravelEloquentSequencer\Tests\Factories\Factory;
+use Gurgentil\LaravelEloquentSequencer\SequencingStrategy;
+use Gurgentil\LaravelEloquentSequencer\Tests\TestCase;
+
+class StrategyOnCreateTest extends TestCase
+{
+    /**
+     * @test
+     * @group Strategy
+     */
+    public function strategy_set_to_on_create_enables_sequencing_on_create()
+    {
+        config(['eloquentsequencer.strategy' => SequencingStrategy::ON_CREATE]);
+
+        $group = Factory::of('Group')->create();
+
+        $firstItem = Factory::of('Item')->create(['group_id' => $group->id]);
+
+        $this->assertEquals(1, $firstItem->refresh()->position);
+    }
+
+    /**
+     * @test
+     * @group Strategy
+     */
+    public function strategy_set_to_on_create_disables_sequencing_on_update()
+    {
+        $group = Factory::of('group')->create();
+
+        $firstItem = Factory::of('item')->create(['group_id' => $group->id]);
+        $secondItem = Factory::of('item')->create(['group_id' => $group->id]);
+
+        config(['eloquentsequencer.strategy' => SequencingStrategy::ON_CREATE]);
+
+        $secondItem->update(['position' => 1]);
+
+        $this->assertEquals(1, $firstItem->refresh()->position);
+        $this->assertEquals(1, $secondItem->refresh()->position);
+    }
+}
