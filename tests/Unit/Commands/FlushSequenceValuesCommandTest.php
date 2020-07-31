657387b84525dd858548d3faff72fbfec865c6b3
Gustavo Gentil <gustavorgentil@outlook.com>
2020-07-31 18:02:18 -0300

    fix: group class name uppercase

--- tests/Unit/Commands/FlushSequenceValuesCommandTest.php
+++ tests/Unit/Commands/FlushSequenceValuesCommandTest.php
@@ -29,7 +29,7 @@ class FlushSequenceValuesCommandTest extends TestCase
     /** @test */
     public function the_flush_command_does_not_update_values_that_are_already_null()
     {
-        $group = Factory::of('group')->create();
+        $group = Factory::of('Group')->create();
 
         $items = Factory::of('Item')->times(4)->create(['group_id' => $group->id]);
 
@@ -48,7 +48,7 @@ class FlushSequenceValuesCommandTest extends TestCase
     /** @test */
     public function the_flush_command_flushes_sequence_values()
     {
-        $group = Factory::of('group')->create();
+        $group = Factory::of('Group')->create();
 
         $firstItem = Factory::of('Item')->create(['group_id' => $group->id]);
         $secondItem = Factory::of('Item')->create(['group_id' => $group->id]);
--- tests/Unit/Commands/PopulateSequenceValuesCommandTest.php
+++ tests/Unit/Commands/PopulateSequenceValuesCommandTest.php
@@ -29,7 +29,7 @@ class PopulateSequenceValuesCommandTest extends TestCase
     /** @test */
     public function the_populate_command_does_not_update_values_that_are_not_null()
     {
-        $group = Factory::of('group')->create();
+        $group = Factory::of('Group')->create();
 
         $items = Factory::of('Item')
             ->times(4)
@@ -48,7 +48,7 @@ class PopulateSequenceValuesCommandTest extends TestCase
     /** @test */
     public function the_populate_command_populates_every_empty_sequence_value()
     {
-        $group = Factory::of('group')->create();
+        $group = Factory::of('Group')->create();
 
         $firstItem = Factory::of('Item')->create(['group_id' => $group->id]);
         $secondItem = Factory::of('Item')->create(['group_id' => $group->id]);
