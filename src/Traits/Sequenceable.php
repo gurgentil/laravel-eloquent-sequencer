d758f71372941687a5c78c303f4453f4410fff1b
Richard van Baarsen <baarsen@pm.me>
2020-10-03 10:50:29 +0200

    Remove unused import

--- src/Traits/Sequenceable.php
+++ src/Traits/Sequenceable.php
@@ -7,7 +7,6 @@ use Gurgentil\LaravelEloquentSequencer\SequencingStrategy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
 
 trait Sequenceable
 {
