d599fe738508df4e8aee06dcead6e4e7c1e32056
Gustavo R. Gentil <gustavorgentil@outlook.com>
2020-11-18 03:51:24 -0300

    refactor: move tests database configuration to phpunit.xml (#22)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,57 +2,22 @@
 
 namespace Gurgentil\LaravelEloquentSequencer\Tests;
 
-use Gurgentil\LaravelEloquentSequencer\LaravelEloquentSequencerServiceProvider;
+use Gurgentil\LaravelEloquentSequencer\ServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->setUpDatabase();
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        parent::tearDownAfterClass();
-
-        static::resetDatabase();
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            LaravelEloquentSequencerServiceProvider::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'sqlite');
-
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => static::getTempDirectory().'/database.sqlite',
-            'prefix' => '',
-        ]);
-    }
-
-    protected function setUpDatabase()
-    {
-        static::resetDatabase();
-
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-    }
-
-    protected static function resetDatabase()
-    {
-        file_put_contents(static::getTempDirectory().'/database.sqlite', null);
-    }
-
-    protected static function getTempDirectory(): string
-    {
-        return __DIR__.'/temp';
     }
 }
