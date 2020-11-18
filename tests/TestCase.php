1f190795e07171c17ba059a7eadeb9bf0e5a63ab
Gustavo Gentil <gustavorgentil@outlook.com>
2020-11-17 23:39:48 -0300

    refactor: move testing database configuration to phpunit.xml

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,52 +7,17 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->setUpDatabase();
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        parent::tearDownAfterClass();
-
-        static::resetDatabase();
-    }
-
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             LaravelEloquentSequencerServiceProvider::class,
         ];
     }
 
-    protected function getEnvironmentSetUp($app)
+    protected function setUp(): void
     {
-        $app['config']->set('database.default', 'sqlite');
-
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => static::getTempDirectory().'/database.sqlite',
-            'prefix' => '',
-        ]);
-    }
-
-    protected function setUpDatabase()
-    {
-        static::resetDatabase();
+        parent::setUp();
 
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-    }
-
-    protected static function resetDatabase()
-    {
-        file_put_contents(static::getTempDirectory().'/database.sqlite', null);
-    }
-
-    protected static function getTempDirectory(): string
-    {
-        return __DIR__.'/temp';
     }
 }
