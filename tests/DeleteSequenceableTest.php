280248105885b7b44c37d3d10df527d5eac763e5
Gustavo Gentil <gustavorgentil@outlook.com>
2021-01-17 19:48:47 -0300

    refactor: create method in test case class to help with assertions

--- tests/DeleteSequenceableTest.php
+++ tests/DeleteSequenceableTest.php
@@ -2,60 +2,59 @@
 
 namespace Gurgentil\LaravelEloquentSequencer\Tests;
 
-use Facades\Gurgentil\LaravelEloquentSequencer\Tests\Factories\Factory;
-
 class DeleteSequenceableTest extends TestCase
 {
     /** @test */
     public function when_an_element_is_deleted_all_succeeding_siblings_should_be_updated(): void
     {
-        $group = Factory::of('Group')->create();
+        $sequence = $this->createSequence();
 
-        $firstItem = Factory::of('Item')->create(['group_id' => $group->id]);
-        $secondItem = Factory::of('Item')->create(['group_id' => $group->id]);
-        $thirdItem = Factory::of('Item')->create(['group_id' => $group->id]);
+        $firstItem = $this->createSequenceable($sequence);
+        $secondItem = $this->createSequenceable($sequence);
+        $thirdItem = $this->createSequenceable($sequence);
 
         $secondItem->delete();
 
-        self::assertEquals(1, $firstItem->refresh()->position);
-        self::assertEquals(2, $thirdItem->refresh()->position);
+        $this->assertSequenced([
+            $firstItem,
+            $thirdItem,
+        ]);
 
         $firstItem->delete();
 
-        self::assertEquals(1, $thirdItem->refresh()->position);
+        $this->assertSequenced([$thirdItem]);
     }
 
     /** @test */
     public function when_all_elements_are_deleted_and_a_new_one_is_created_it_is_assigned_the_value_of_1(): void
     {
-        $group = Factory::of('Group')->create();
+        $sequence = $this->createSequence();
 
-        $deletedItem = Factory::of('Item')->create(['group_id' => $group->id]);
+        $this->createSequenceable($sequence)
+            ->delete();
 
-        $deletedItem->delete();
+        $newlyCreatedItem = $this->createSequenceable($sequence);
 
-        $this->assertCount(0, $group->refresh()->items);
-
-        $newItem = Factory::of('Item')->create(['group_id' => $group->id]);
-
-        $this->assertEquals(1, $newItem->position);
+        $this->assertSequenced([$newlyCreatedItem]);
     }
 
     /** @test */
-    public function the_sequence_group_is_not_affected_when_an_element_is_deleted_in_another_sequence(): void
+    public function the_sequence_is_not_affected_when_an_element_is_deleted_in_another_sequence(): void
     {
-        $group = Factory::of('Group')->create();
-        $anotherGroup = Factory::of('Group')->create();
+        $sequence = $this->createSequence();
+        $anotherSequence = $this->createSequence();
 
-        $firstItem = Factory::of('Item')->create(['group_id' => $group->id]);
-        $secondItem = Factory::of('Item')->create(['group_id' => $group->id]);
+        $firstItem = $this->createSequenceable($sequence);
+        $secondItem = $this->createSequenceable($sequence);
 
-        $firstFromTheOtherGroup = Factory::of('Item')->create(['group_id' => $anotherGroup->id]);
+        $firstItemFromAnotherSequence = $this->createSequenceable($anotherSequence);
 
-        $firstFromTheOtherGroup->delete();
+        $firstItemFromAnotherSequence->delete();
 
-        $this->assertCount(2, $group->refresh()->items);
-        $this->assertEquals(1, $firstItem->refresh()->position);
-        $this->assertEquals(2, $secondItem->refresh()->position);
+        $this->assertSequenceableCount($sequence, 2);
+        $this->assertSequenced([
+            $firstItem,
+            $secondItem,
+        ]);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Gurgentil\LaravelEloquentSequencer\Tests;
 
+use Facades\Gurgentil\LaravelEloquentSequencer\Tests\Factories\Factory;
 use Gurgentil\LaravelEloquentSequencer\ServiceProvider;
+use Gurgentil\LaravelEloquentSequencer\Tests\Models\Group;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
@@ -19,5 +21,40 @@ class TestCase extends OrchestraTestCase
         parent::setUp();
 
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+    }
+
+    protected function createSequenceable(Group $sequence, int $amount = 1)
+    {
+        $factory = Factory::of('Item');
+
+        if ($amount === 1) {
+            return $factory->create(['group_id' => $sequence->id]);
+        }
+
+        return $factory->times($amount)
+            ->create(['group_id' => $sequence->id]);
+    }
+
+    protected function createSequence()
+    {
+        return Factory::of('Group')->create();
+    }
+
+    protected function assertSequenced(array $sequenceables): self
+    {
+        collect($sequenceables)->each(function ($sequenceable, $index) {
+            $expectedPosition = $index + 1;
+
+            self::assertEquals($expectedPosition, $sequenceable->refresh()->position);
+        });
+
+        return $this;
+    }
+
+    protected function assertSequenceableCount(Group $sequence, int $expectedCount): self
+    {
+        self::assertCount($expectedCount, $sequence->refresh()->items);
+
+        return $this;
     }
 }
