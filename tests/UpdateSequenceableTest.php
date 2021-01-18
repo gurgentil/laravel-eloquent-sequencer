a58518df6743163f31465d88a3d1f5941e75612d
Gustavo Gentil <gustavorgentil@outlook.com>
2021-01-17 23:27:24 -0300

    refactor: rename update tests

--- tests/UpdateSequenceableTest.php
+++ tests/UpdateSequenceableTest.php
@@ -7,7 +7,29 @@ use Gurgentil\LaravelEloquentSequencer\Exceptions\SequenceValueOutOfBoundsExcept
 class UpdateSequenceableTest extends TestCase
 {
     /** @test */
-    public function it_updates_4th_object_with_sequence_value_equal_to_2_and_move_the_group_around(): void
+    public function it_moves_an_element_towards_the_end_of_the_sequence(): void
+    {
+        $sequence = $this->createSequence();
+
+        $firstItem = $this->createSequenceable($sequence);
+        $secondItem = $this->createSequenceable($sequence);
+        $thirdItem = $this->createSequenceable($sequence);
+        $fourthItem = $this->createSequenceable($sequence);
+        $fifthItem = $this->createSequenceable($sequence);
+
+        $firstItem->update(['position' => 3]);
+
+        $this->assertSequenced([
+            $secondItem,
+            $thirdItem,
+            $firstItem,
+            $fourthItem,
+            $fifthItem,
+        ]);
+    }
+
+    /** @test */
+    public function it_moves_an_element_towards_the_start_of_the_sequence(): void
     {
         $sequence = $this->createSequence();
 
@@ -29,125 +51,13 @@ class UpdateSequenceableTest extends TestCase
     }
 
     /** @test */
-    public function it_updates_5th_object_with_sequence_value_equal_to_2_and_move_the_group_around(): void
+    public function it_does_not_change_the_sequence_when_the_updated_element_gets_a_value_equal_to_its_current_value(): void
     {
         $sequence = $this->createSequence();
 
         $firstItem = $this->createSequenceable($sequence);
         $secondItem = $this->createSequenceable($sequence);
         $thirdItem = $this->createSequenceable($sequence);
-        $fourthItem = $this->createSequenceable($sequence);
-        $fifthItem = $this->createSequenceable($sequence);
-
-        $fifthItem->update(['position' => 2]);
-
-        $this->assertSequenced([
-            $firstItem,
-            $fifthItem,
-            $secondItem,
-            $thirdItem,
-            $fourthItem,
-        ]);
-    }
-
-    /** @test */
-    public function it_updates_3rd_object_with_sequence_value_equal_to_1_and_move_the_group_around(): void
-    {
-        $sequence = $this->createSequence();
-
-        $firstItem = $this->createSequenceable($sequence);
-        $secondItem = $this->createSequenceable($sequence);
-        $thirdItem = $this->createSequenceable($sequence);
-        $fourthItem = $this->createSequenceable($sequence);
-        $fifthItem = $this->createSequenceable($sequence);
-
-        $thirdItem->update(['position' => 1]);
-
-        $this->assertSequenced([
-            $thirdItem,
-            $firstItem,
-            $secondItem,
-            $fourthItem,
-            $fifthItem,
-        ]);
-    }
-
-    /** @test */
-    public function it_updates_2nd_object_with_sequence_value_equal_to_4_and_move_the_group_around(): void
-    {
-        $sequence = $this->createSequence();
-
-        $firstItem = $this->createSequenceable($sequence);
-        $secondItem = $this->createSequenceable($sequence);
-        $thirdItem = $this->createSequenceable($sequence);
-        $fourthItem = $this->createSequenceable($sequence);
-        $fifthItem = $this->createSequenceable($sequence);
-
-        $secondItem->update(['position' => 4]);
-
-        $this->assertSequenced([
-            $firstItem,
-            $thirdItem,
-            $fourthItem,
-            $secondItem,
-            $fifthItem,
-        ]);
-    }
-
-    /** @test */
-    public function it_updates_1st_object_with_sequence_value_equal_to_4_and_move_the_group_around(): void
-    {
-        $sequence = $this->createSequence();
-
-        $firstItem = $this->createSequenceable($sequence);
-        $secondItem = $this->createSequenceable($sequence);
-        $thirdItem = $this->createSequenceable($sequence);
-        $fourthItem = $this->createSequenceable($sequence);
-        $fifthItem = $this->createSequenceable($sequence);
-
-        $firstItem->update(['position' => 4]);
-
-        $this->assertSequenced([
-            $secondItem,
-            $thirdItem,
-            $fourthItem,
-            $firstItem,
-            $fifthItem,
-        ]);
-    }
-
-    /** @test */
-    public function it_updates_2nd_object_with_sequence_value_equal_to_5_and_move_the_group_around(): void
-    {
-        $sequence = $this->createSequence();
-
-        $firstItem = $this->createSequenceable($sequence);
-        $secondItem = $this->createSequenceable($sequence);
-        $thirdItem = $this->createSequenceable($sequence);
-        $fourthItem = $this->createSequenceable($sequence);
-        $fifthItem = $this->createSequenceable($sequence);
-
-        $secondItem->update(['position' => 5]);
-
-        $this->assertSequenced([
-            $firstItem,
-            $thirdItem,
-            $fourthItem,
-            $fifthItem,
-            $secondItem,
-        ]);
-    }
-
-    /** @test */
-    public function it_does_not_need_to_update_the_sequence_when_the_updated_object_receives_a_value_equal_to_its_current_value(): void
-    {
-        $sequence = $this->createSequence();
-
-        $firstItem = $this->createSequenceable($sequence);
-        $secondItem = $this->createSequenceable($sequence);
-        $thirdItem = $this->createSequenceable($sequence);
-
-        $this->assertSequenceValue($secondItem, 2);
 
         $secondItem->update(['position' => 2]);
 
@@ -158,8 +68,11 @@ class UpdateSequenceableTest extends TestCase
         ]);
     }
 
-    /** @test */
-    public function when_a_sequence_is_updated_the_first_object_will_have_the_initial_value(): void
+    /**
+     * @test
+     * @group InitialValue
+     */
+    public function when_a_sequence_is_updated_the_first_element_will_receive_the_predefined_initial_value(): void
     {
         config(['eloquentsequencer.initial_value' => 10]);
 
@@ -178,8 +91,11 @@ class UpdateSequenceableTest extends TestCase
         ], 10);
     }
 
-    /** @test */
-    public function it_throws_an_exception_when_the_object_updated_has_a_sequence_value_that_is_negative(): void
+    /**
+     * @test
+     * @group SequenceValueOutOfBoundsException
+     */
+    public function it_throws_an_exception_when_the_updated_element_has_a_negative_sequence_value(): void
     {
         $sequence = $this->createSequence();
 
@@ -190,8 +106,11 @@ class UpdateSequenceableTest extends TestCase
         $item->update(['position' => -1]);
     }
 
-    /** @test */
-    public function it_throws_an_exception_when_the_object_updated_has_a_sequence_value_equal_to_zero(): void
+    /**
+     * @test
+     * @group SequenceValueOutOfBoundsException
+     */
+    public function it_throws_an_exception_when_the_updated_element_has_a_sequence_value_equal_to_zero(): void
     {
         $sequence = $this->createSequence();
 
@@ -202,8 +121,12 @@ class UpdateSequenceableTest extends TestCase
         $item->update(['position' => 0]);
     }
 
-    /** @test */
-    public function it_throws_an_exception_when_the_object_updated_has_a_sequence_value_greater_than_the_last_value(): void
+    /**
+     * @test
+     * @group LastValue
+     * @group SequenceValueOutOfBoundsException
+     */
+    public function it_throws_an_exception_when_the_updated_element_has_a_sequence_value_greater_than_the_last_value(): void
     {
         $sequence = $this->createSequence();
 
@@ -215,8 +138,12 @@ class UpdateSequenceableTest extends TestCase
         $item->update(['position' => 3]);
     }
 
-    /** @test */
-    public function it_throws_an_exception_when_the_object_updated_has_a_sequence_value_greater_than_the_next_value(): void
+    /**
+     * @test
+     * @group NextValue
+     * @group SequenceValueOutOfBoundsException
+     */
+    public function it_throws_an_exception_when_the_updated_element_has_a_sequence_value_greater_than_the_next_value(): void
     {
         $sequence = $this->createSequence();
 
@@ -228,8 +155,12 @@ class UpdateSequenceableTest extends TestCase
         $item->update(['position' => 4]);
     }
 
-    /** @test */
-    public function it_throws_an_exception_when_the_updated_object_has_a_sequence_value_greater_than_the_next_value_and_is_currently_set_to_null(): void
+    /**
+     * @test
+     * @group NextValue
+     * @group SequenceValueOutOfBoundsException
+     */
+    public function it_throws_an_exception_when_the_updated_element_has_a_sequence_value_greater_than_the_next_value_and_is_currently_set_to_null(): void
     {
         $sequence = $this->createSequence();
 
@@ -245,8 +176,12 @@ class UpdateSequenceableTest extends TestCase
         $item->update(['position' => 4]);
     }
 
-    /** @test */
-    public function it_does_not_throw_an_exception_when_the_updated_object_has_a_sequence_value_equal_to_the_next_value_and_is_currently_set_to_null(): void
+    /**
+     * @test
+     * @group NextValue
+     * @group SequenceValueOutOfBoundsException
+     */
+    public function it_does_not_throw_an_exception_when_the_updated_element_has_a_sequence_value_equal_to_the_next_value_and_is_currently_set_to_null(): void
     {
         $sequence = $this->createSequence();
 
@@ -255,15 +190,19 @@ class UpdateSequenceableTest extends TestCase
 
         $item->update(['position' => null]);
 
-        self::assertNull($item->refresh()->position);
+        self::assertNull($item->position);
 
         $item->update(['position' => 2]);
 
         $this->assertSequenceValue($item, 2);
     }
 
-    /** @test */
-    public function it_throws_an_exception_when_the_updated_object_has_a_sequence_value_smaller_than_the_initial_value(): void
+    /**
+     * @test
+     * @group SequenceValueOutOfBoundsException
+     * @group InitialValue
+     */
+    public function it_throws_an_exception_when_the_updated_element_has_a_sequence_value_smaller_than_the_initial_value(): void
     {
         config(['eloquentsequencer.initial_value' => 10]);
 
